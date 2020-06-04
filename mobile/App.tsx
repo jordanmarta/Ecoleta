e8ead1be0bfdb6f825e1df3002482975493a8220
jordan <jordan@gmail.com>
2020-06-04 12:55:15 -0300

    mobile da aplicação

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Hello, Jordan!</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#7159c1',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+
+	text: {
+		fontSize: 20,
+		color: '#FFF'
+	}
+});
